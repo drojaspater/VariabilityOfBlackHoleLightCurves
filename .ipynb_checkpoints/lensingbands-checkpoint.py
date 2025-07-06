2edf9a3515f45cec64d4e164602f62fff7d206ad
CorolarioPater <drojasp@unal.edu.co>
2025-07-06 10:45:06 -0500

    I correct the error

--- .ipynb_checkpoints/lensingbands-checkpoint.py
+++ .ipynb_checkpoints/lensingbands-checkpoint.py
@@ -1,9 +1,9 @@
 from aart_func import *
 from params import * 
 
-spin_case = float(sys.argv[1])
-i_case = float(sys.argv[2])
-dx0 = float(sys.argv[3])
+#spin_case = float(sys.argv[1])
+#i_case = float(sys.argv[2])
+#dx0 = float(sys.argv[3])
 
 print("Computing the lensing bands")
 lb.lb()
