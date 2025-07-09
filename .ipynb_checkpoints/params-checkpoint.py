832c76361c985dd2e53c94d3693e1034414ce459
CorolarioPater <drojasp@unal.edu.co>
2025-07-09 09:12:15 -0500

    New Parameter

--- .ipynb_checkpoints/params-checkpoint.py
+++ .ipynb_checkpoints/params-checkpoint.py
@@ -39,11 +39,11 @@ bvapp=0
 p_image=1
 limits=25
 #Resolution for the n=0 image [M]
-dx0 =1
+dx0 =5.0
 #Resolution for the n=1 image [M]
-dx1 =1
+dx1 =5.0
 #Resolution for the n=2 image [M]
-dx2 =1
+dx2 =5.0
 
 # Projection angle for the radon transformation
 
