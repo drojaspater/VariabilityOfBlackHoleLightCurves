86b69e73c045da5993cd05bc2316df6a581c25e9
CorolarioPater <drojasp@unal.edu.co>
2025-07-23 11:41:16 -0500

    Error

--- .ipynb_checkpoints/CurveSim-checkpoint.py
+++ .ipynb_checkpoints/CurveSim-checkpoint.py
@@ -1,7 +1,7 @@
 from skimage.metrics import structural_similarity as ssim
 from params import *
 from aart_func import *
-import numpy as pd
+import pandas as pd
 
 def similarity_by_ssim(img1, img2):
     # Asegúrate de especificar el rango de datos (data_range)
