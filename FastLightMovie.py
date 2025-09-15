12bdf07eeb81778ebea1935344c000ec49157bc2
CorolarioPater <drojasp@unal.edu.co>
2025-09-14 19:53:35 -0500

    I change the interpolation in fast light. Now we can interpolate in time with the porpuse of do the slow-light in any time of the curve

--- FastLightMovie.py
+++ FastLightMovie.py
@@ -129,7 +129,6 @@ maxintensity=np.nanmax(data)
 interpolated3_R=RegularGridInterpolator((times,x1,x2),data,fill_value=0,bounds_error=False,method='linear')
 
 def MovieWorker(tsnap):
-    # Definir interpolador espacial que usa el 3D global
     def spatial_interpolator(x_coords, y_coords):
         points = np.column_stack([
             np.full(len(x_coords), tsnap),
@@ -138,16 +137,15 @@ def MovieWorker(tsnap):
         ])
         return interpolated3_R(points)
 
-	i_bghts0 = obsint.fast_light(supergrid0,mask0,sign0,spin_case,isco,rs0,phi0, spatial_interpolator,thetao)
-	i_bghts1 = obsint.fast_light(supergrid1,mask1,sign1,spin_case,isco,rs1,phi1, spatial_interpolator,thetao)
-	i_bghts2 = obsint.fast_light(supergrid2,mask2,sign2,spin_case,isco,rs2,phi2, spatial_interpolator,thetao)
+    
+    i_bghts0 = obsint.fast_light(supergrid0,mask0,sign0,spin_case,isco,rs0,phi0, spatial_interpolator,thetao)
+    i_bghts1 = obsint.fast_light(supergrid1,mask1,sign1,spin_case,isco,rs1,phi1, spatial_interpolator,thetao)
+    i_bghts2 = obsint.fast_light(supergrid2,mask2,sign2,spin_case,isco,rs2,phi2, spatial_interpolator,thetao)
 
-	i_I0 = (i_bghts0).reshape(N0,N0).T
-	i_I1 = (i_bghts1).reshape(N1,N1).T
-	i_I2 = (i_bghts2).reshape(N2,N2).T
-
-	print("Calculating an image at time t=%s (M)"%np.round(tsnap*dt,5))
-	return(i_I0,i_I1,i_I2)
+    i_I0 = (i_bghts0).reshape(N0,N0).T
+    i_I1 = (i_bghts1).reshape(N1,N1).T
+    i_I2 = (i_bghts2).reshape(N2,N2).T
+    
 
 I0s = []
 I1s = []
