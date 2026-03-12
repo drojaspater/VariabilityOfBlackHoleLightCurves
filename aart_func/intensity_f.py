6a635212293d8f619d8c9cbe12b0076a454db4a9
CorolarioPater <drojasp@unal.edu.co>
2026-03-11 21:57:28 -0500

    Prove of the time without nan

--- aart_func/intensity_f.py
+++ aart_func/intensity_f.py
@@ -350,7 +350,14 @@ def slow_light(grid,mask,redshift_sign,a,isco,rs,th,ts,interpolation,thetao):
     th = th[mask]
     ts = ts[mask]
 
-    ######################################
+    ################# without nan #####################
+    valid_mask = np.isfinite(ts)
+    alpha = grid[:,0][valid_mask]
+    beta = grid[:,1][valid_mask]
+    rs = rs[valid_mask]
+    th = th[valid_mask]
+    ts = ts[valid_mask]
+    ################# Time Filter #####################
     MaskTime = Mask_FilterTime(ts)
     alpha = grid[:,0][MaskTime]
     beta = grid[:,1][MaskTime]
