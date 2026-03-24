6ba3ba5ae9fa3e4cc111cff822b1d0fc8b069078
CorolarioPater <drojasp@unal.edu.co>
2026-03-23 22:43:00 -0500

    Picture validation with some things that not work

---
+++ PictureValidation.py
@@ -0,0 +1,172 @@
+import subprocess
+import pandas as pd
+from aart_func import *
+from params import * 
+
+# Creation of the .h5 data
+filename = "LensingBands_a%s_i%s_dx%s.h5"%(spin_case, i_case, dx0)
+filepath = os.path.join(path_lb, filename)
+if os.path.exists(filepath):
+    print(f"File {filename} already exists. Skipping...")
+else:
+    print(f"File {filename} not found. Running lensingbands.py...")
+    subprocess.run(["python", "lensingbands.py"])
+
+filename = "Rays_a%s_i%s_dx%s.h5"%(spin_case,i_case,dx0)
+filepath = os.path.join(path_rt, filename)
+if os.path.exists(filepath):
+    print(f"File {filename} already exists. Skipping...")
+else:
+    print(f"File {filename} not found. Running raytracing.py...")
+    subprocess.run(["python", "raytracing.py"])
+
+# Lecuture of inoisy
+print("Reading inoisy file: ",path_InoisyEnvelope+i_fname)
+
+hf = h5py.File(path_InoisyEnvelope+i_fname, 'r')
+
+data = np.array(hf['data/data_env'])
+#inoisy has periodic boudaries, so we need to copy wrap the data with one frame
+data=np.concatenate((data,data[0,:,:][np.newaxis,:,:]),axis=0)
+data=np.flip(data,axis=(2))
+
+nt = data.shape[0] #inoisy time resolution
+ni = data.shape[1] #inoisy x resolution
+nj = data.shape[2] #inoisy y resolution
+
+try: 
+	xtstart = np.array(hf['params/x0start'])[0]
+	xtend = np.array(hf['params/x0end'])[0]
+
+	x1start = np.array(hf['params/x1start'])[0]
+	x2start = np.array(hf['params/x2start'])[0]
+
+	x1end = np.array(hf['params/x1end'])[0]
+	x2end = np.array(hf['params/x2end'])[0]
+
+except:
+	xtstart = np.array(hf['params/x0start'])
+	xtend = np.array(hf['params/x0end'])
+
+	x1start = np.array(hf['params/x1start'])
+	x2start = np.array(hf['params/x2start'])
+
+	x1end = np.array(hf['params/x1end'])
+	x2end = np.array(hf['params/x2end'])
+
+
+x1 = np.linspace(x1start, x1end, ni) 
+x2 = np.linspace(x2start, x2end, nj)
+
+times = np.linspace(xtstart, xtend, nt) 
+
+h5py.File.close(hf)
+
+
+##Fast Light 
+print("Computing the fast-light single picture")
+
+fnbands=path_lb+"LensingBands_a%s_i%s_dx%s.h5"%(spin_case,i_case,dx0)
+
+print("Reading file: ",fnbands)
+
+h5f = h5py.File(fnbands,'r')
+
+supergrid0=h5f['grid0'][:]
+mask0=h5f['mask0'][:]
+N0=int(h5f["N0"][0])
+
+supergrid1=h5f['grid1'][:]
+mask1=h5f['mask1'][:]
+N1=int(h5f["N1"][0])
+	
+supergrid2=h5f['grid2'][:]
+mask2=h5f['mask2'][:]
+N2=int(h5f["N2"][0])
+
+h5f.close()
+
+fnbands=path_rt+"Rays_a%s_i%s_dx%s.h5"%(spin_case,i_case,dx0)
+
+print("Reading file: ",fnbands)
+
+h5f = h5py.File(fnbands,'r')
+
+rs0=h5f['rs0'][:]
+sign0=h5f['sign0'][:]
+t0=h5f['t0'][:]
+phi0=h5f['phi0'][:]
+
+rs1=h5f['rs1'][:]
+sign1=h5f['sign1'][:]
+t1=h5f['t1'][:]
+phi1=h5f['phi1'][:]
+
+rs2=h5f['rs2'][:]
+sign2=h5f['sign2'][:]
+t2=h5f['t2'][:]
+phi2=h5f['phi2'][:]
+
+h5f.close()
+
+fact=-(D_obs+2*np.log(D_obs))
+
+t0-=fact
+t1-=fact
+t2-=fact
+
+
+print("AART starts!")
+
+i_dt = xtend/nt
+timeconversion=i_dt*MMkg*Gc/cc**3/(3600*24) # [days]
+
+maxintensity=np.nanmax(data)
+
+##Tomar el porcentil para poder ver la coordenada más usual 
+
+print("Using a single inoisy frame")
+interpolated2_R0 =RegularGridInterpolator((x1,x2), data[i_frame,:,:],fill_value=0,bounds_error=False,method='linear')
+interpolated2_R1 =RegularGridInterpolator((x1,x2), data[i_frame -int(f_tM/17),:,:],fill_value=0,bounds_error=False,method='linear')
+interpolated2_R2 =RegularGridInterpolator((x1,x2), data[i_frame -int(f_tM/32),:,:],fill_value=0,bounds_error=False,method='linear')
+
+dt = f_tM/snapshots_source
+
+print("Computing a lensed image")
+i_bghts0 = obsint.fast_light(supergrid0,mask0,sign0,spin_case,isco,rs0,phi0, interpolated2_R0,thetao)
+i_bghts1 = obsint.fast_light(supergrid1,mask1,sign1,spin_case,isco,rs1,phi1, interpolated2_R1,thetao)
+i_bghts2 = obsint.fast_light(supergrid2,mask2,sign2,spin_case,isco,rs2,phi2, interpolated2_R2,thetao)
+i_I0 = (i_bghts0).reshape(N0,N0).T
+i_I1 = (i_bghts1).reshape(N1,N1).T
+i_I2 = (i_bghts2).reshape(N2,N2).T
+
+filename=path+"Image_a_%s_i_%s_%frame.h5"%(spin_case,i_case,i_frame)
+h5f = h5py.File(filename, 'w')
+h5f.create_dataset('bghts0', data=i_I0)
+h5f.create_dataset('bghts1', data=i_I1)
+h5f.create_dataset('bghts2', data=i_I2)
+h5f.close()
+print("Single image file ",filename," created.\n")
+
+
+
+
+print("Using all the available inoisy frames")
+interpolated3_R=RegularGridInterpolator((times,x1,x2), data,fill_value=0,bounds_error=False,method='linear')
+print("Computing lensed image using all inoisy frames")
+i_bghts0 = obsint.slow_light(supergrid0,mask0,sign0,spin_case,isco,rs0,phi0,np.mod(t0+i_tM+i_frame,xtend), interpolated3_R,thetao);
+i_bghts1 = obsint.slow_light(supergrid1,mask1,sign1,spin_case,isco,rs1,phi1,np.mod(t1+i_tM+i_frame,xtend), interpolated3_R,thetao);
+i_bghts2 = obsint.slow_light(supergrid2,mask2,sign2,spin_case,isco,rs2,phi2,np.mod(t2+i_tM+i_frame,xtend), interpolated3_R,thetao);
+i_I0 = (i_bghts0).reshape(N0,N0).T
+i_I1 = (i_bghts1).reshape(N1,N1).T
+i_I2 = (i_bghts2).reshape(N2,N2).T
+filename=path+"Dynamical_Image_a_%s_i_%s.h5"%(spin_case,i_case)
+h5f = h5py.File(filename, 'w')
+h5f.create_dataset('bghts0', data=i_I0)
+h5f.create_dataset('bghts1', data=i_I1)
+h5f.create_dataset('bghts2', data=i_I2)
+h5f.close()
+print("Images file ",filename," created.")
+
+
+
